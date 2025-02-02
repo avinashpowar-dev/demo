c46ba921e5a144e9abbaed25a29f79879c729f93
Avinash Powar <avinash.powar@gmail.com>
2025-02-02 16:49:35 +0530

    next js project

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,4 @@
 export default async function Products(){
-    //export default async function Products({params}:{params :{id:string}}){
-   //const { id } = await params;
-   //return <h1>Products {id}</h1>
+ 
     return <h1>Products </h1>
 }
