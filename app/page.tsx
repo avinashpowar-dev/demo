0484721408a6645c0e9738c8ce8522ab6ce1657f
Avinash Powar <avinash.powar@gmail.com>
2025-02-02 16:31:37 +0530

    next js project

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Users  from "./mock-users/page";
+
 
 
 export default async function Home() {
@@ -7,8 +7,8 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Current pathname: 
-      <Users/>
+      Current pathname: Avinash
+      
        
 
       </main>
